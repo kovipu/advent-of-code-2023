f2c81a600e88ed492a1813f829617f5156dc616e
Konsta Purtsi <konsta@arado.fi>
2023-12-01 11:23:51 +0200

    day 1 part 1

--- src/Days/Day01.hs
+++ src/Days/Day01.hs
@@ -1,19 +1,21 @@
 module Days.Day01 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import           Data.Char
+import           Data.Map.Strict      (Map)
+import qualified Data.Map.Strict      as Map
+import           Data.Maybe
+import           Data.Set             (Set)
+import qualified Data.Set             as Set
+import           Data.Text            (Text)
+import           Data.Text            as T
+import           Data.Vector          (Vector)
+import qualified Data.Vector          as Vec
+import qualified Util.Util            as U
 
-import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
-import Data.Void
+import           Data.Attoparsec.Text
+import           Data.Void
+import qualified Program.RunDay       as R (Day, runDay)
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,18 +23,24 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = (takeWhile1 (/= '\n')) `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Text]
 
-type OutputA = Void
+type OutputA = Maybe Int
 
 type OutputB = Void
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA input = sum <$> traverse matchLine input
+
+matchLine :: Text -> Maybe Int
+matchLine line = do
+  first <- T.find isDigit line
+  last <- T.find isDigit $ T.reverse line
+  pure $ read [first, last]
 
 ------------ PART B ------------
 partB :: Input -> OutputB
