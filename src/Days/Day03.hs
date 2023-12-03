ebf460925ca7ee7508985fe00eb3d908ee0a9668
Konsta Purtsi <konsta@arado.fi>
2023-12-03 10:39:20 +0200

    Day 3 part 1

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -1,19 +1,24 @@
 module Days.Day03 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import           Data.Char               (isDigit)
+import           Data.Foldable.WithIndex
+import           Data.List
+import           Data.Map.Strict         (Map)
+import qualified Data.Map.Strict         as Map
+import           Data.Maybe
+import           Data.Set                (Set)
+import qualified Data.Set                as Set
+import           Data.Vector             (Vector)
+import qualified Data.Vector             as Vec
+import           Linear.V2               (V2 (..))
+import qualified Util.Util               as U
 
-import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
-import Data.Void
+import           Data.Attoparsec.Text
+import           Data.Text               (Text)
+import qualified Data.Text               as T
+import           Data.Void
+import qualified Program.RunDay          as R (Day, runDay)
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,18 +26,91 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = (takeWhile1 (/= '\n')) `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Text]
 
-type OutputA = Void
+data Schem
+  = Part Int (V2 Int, V2 Int)
+  | Symbol Char (V2 Int)
+  deriving Show
+
+type Schems = [Schem]
+
+type OutputA = Int
 
 type OutputB = Void
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA input =
+  let schems = mankeloi input
+   in foldl
+      (isPartNumber schems)
+      0
+      schems
+
+isPartNumber :: Schems -> Int -> Schem -> Int
+isPartNumber schems acc schem =
+  case schem of
+    Symbol _ _ -> acc
+    Part n (start, end) ->
+      -- search around start & end for symbols
+      if search schems start || search schems end
+         then acc + n
+         else acc
+
+search :: Schems -> V2 Int -> Bool
+search schems coords =
+  isJust $ find
+    (\schem ->
+      case schem of
+        Part _ _              -> False
+        Symbol _ symbolCoords -> isAdjacent coords symbolCoords
+    )
+    schems
+
+
+isAdjacent :: V2 Int -> V2 Int -> Bool
+isAdjacent (V2 x1 y1) (V2 x2 y2) = abs (x1 - x2) <= 1 && abs (y1 - y2) <= 1
+
+mankeloi :: [Text] -> Schems
+mankeloi input =
+  ifoldl
+    (\y acc line -> (readLine y line) <> acc)
+    []
+    input
+
+readLine :: Int -> Text -> Schems
+readLine y line =
+  snd $ ifoldl (\x (prev, acc) char ->
+    if isDigit char
+      then case prev of
+        -- start a new integer
+        Symbol _ _ ->
+          let value = read [char]
+              start = V2 x y
+              part = Part value (start, start)
+           in (part, part : acc)
+        -- if prev was an int -> append to that decimal int
+        Part n (start, _end) ->
+          let
+            value = 10 * n + (read [char])
+            part = Part value (start, V2 x y)
+           in (part, part : (drop 1 acc))
+    else
+      let
+        coords = V2 x y
+        part = Symbol char coords
+       in if char == '.'
+            then (part, acc)
+            else (part, part : acc)
+  )
+  (Symbol '.' (V2 0 0), [])
+  (T.unpack line)
+
+
 
 ------------ PART B ------------
 partB :: Input -> OutputB
