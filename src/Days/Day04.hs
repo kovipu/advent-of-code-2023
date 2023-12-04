c8feabe04a71b66e71507694da5b912690d0f09f
Konsta Purtsi <konsta@arado.fi>
2023-12-04 08:13:24 +0200

    day 4 parts 1 & 2

--- src/Days/Day04.hs
+++ src/Days/Day04.hs
@@ -1,19 +1,21 @@
 module Days.Day04 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import           Data.List            ()
+import           Data.Map.Strict      (Map)
+import qualified Data.Map.Strict      as Map
+import           Data.Maybe           ()
+import           Data.Set             (Set)
+import qualified Data.Set             as Set
+import           Data.Vector          (Vector)
+import qualified Data.Vector          as Vec
+import qualified Util.Util            as U
 
-import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
-import Data.Void
+import           Control.Applicative  (Alternative (many), optional)
+import           Data.Attoparsec.Text (Parser, char, decimal, endOfLine, sepBy,
+                                       space, string)
+import           Data.Map             ((!))
+import qualified Program.RunDay       as R (Day, runDay)
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +23,64 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = parseLine `sepBy` endOfLine
+
+parseLine :: Parser Card
+parseLine = do
+  string "Card"
+  many space
+  cardId <- decimal
+  char ':'
+  many space
+  winningNumbers <- decimal `sepBy` many space
+  many space
+  char '|'
+  many space
+  myNumbers <- decimal `sepBy` many space
+  pure Card{cardId, winningNumbers, myNumbers}
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Card]
 
-type OutputA = Void
+data Card = Card {cardId :: Int, winningNumbers :: [Int], myNumbers :: [Int]}
+  deriving (Show)
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA cards = sum $ map findPoints cards
+
+findPoints :: Card -> Int
+findPoints card =
+  let n = length $ wins card
+   in case n of
+        0 -> 0
+        _ -> 2 ^ (n - 1)
+
+wins :: Card -> [Int]
+wins Card{winningNumbers, myNumbers} = filter (`elem` winningNumbers) myNumbers
 
 ------------ PART B ------------
-partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB :: Input -> Int
+partB cards =
+  let cardMap = Map.fromList $ map (\card@Card{cardId} -> (cardId, card)) cards
+   in foldl
+        ( \acc card ->
+            acc + wonCards cardMap card
+        )
+        0
+        cards
+
+wonCards :: Map Int Card -> Card -> Int
+wonCards cardMap card@Card{cardId} =
+  let numWins = length $ wins card
+      newCards = if numWins > 0 then [(cardId + 1) .. (cardId + numWins)] else []
+   in foldl
+        ( \acc cid ->
+            acc + wonCards cardMap (cardMap ! cid)
+        )
+        1
+        newCards
