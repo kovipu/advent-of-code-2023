390d3774a983d74847f8974686c9483c2f22bf53
Konsta Purtsi <konsta@arado.fi>
2023-12-05 08:05:06 +0200

    Day 5 part 1

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -1,19 +1,21 @@
 module Days.Day05 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import           Control.Applicative  (empty)
+import           Data.List
+import           Data.Map.Strict      (Map)
+import qualified Data.Map.Strict      as Map
+import           Data.Maybe
+import           Data.Set             (Set)
+import qualified Data.Set             as Set
+import           Data.Vector          (Vector)
+import qualified Data.Vector          as Vec
+import qualified Util.Util            as U
 
-import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
-import Data.Void
+import           Data.Attoparsec.Text
+import           Data.Void
+import           Debug.Trace          (trace)
+import qualified Program.RunDay       as R (Day, runDay)
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,18 +23,66 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+  string "seeds: "
+  seeds <- decimal `sepBy1` space
+  string "\n\n"
+  seedMaps <- parseMap `sepBy1` string "\n\n"
+  pure Input{seeds, seedMaps}
+
+parseMap :: Parser [SeedMap]
+parseMap = do
+  skipWhile (/= ' ')
+  string " map:\n"
+  parseSeedMap `sepBy1` "\n"
+
+parseSeedMap :: Parser SeedMap
+parseSeedMap = do
+  nums <- decimal `sepBy` char ' '
+  case nums of
+    [destRangeStart, srcRangeStart, rangeLength] ->
+      let offset = destRangeStart - srcRangeStart
+       in pure SeedMap{offset, srcRangeStart, rangeLength}
+    _ ->
+      empty
 
 ------------ TYPES ------------
-type Input = Void
+data Input = Input
+  { seeds    :: [Int]
+  , seedMaps :: [[SeedMap]]
+  }
+  deriving (Show)
 
-type OutputA = Void
+data SeedMap = SeedMap
+  { offset        :: Int
+  , srcRangeStart :: Int
+  , rangeLength   :: Int
+  }
+  deriving (Show)
+
+type OutputA = Int
 
 type OutputB = Void
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA Input{seeds, seedMaps} = minimum $ map (getLocation seedMaps) seeds
+
+getLocation :: [[SeedMap]] -> Int -> Int
+getLocation seedMaps seed =
+  foldl
+    getNewDest
+    seed
+    seedMaps
+
+getNewDest :: Int -> [SeedMap] -> Int
+getNewDest seed sMap =
+  fromMaybe
+    seed
+    ( do
+        SeedMap{offset} <- find (\r -> r.srcRangeStart <= seed && seed <= r.srcRangeStart + r.rangeLength) sMap
+        pure $ seed + offset
+    )
 
 ------------ PART B ------------
 partB :: Input -> OutputB
