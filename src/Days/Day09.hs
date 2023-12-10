b96ba31095ea6e326c3d2ec4eaad3bd17e5240ff
Konsta Purtsi <konsta@arado.fi>
2023-12-10 19:53:17 +0200

    Day 9

--- src/Days/Day09.hs
+++ src/Days/Day09.hs
@@ -1,19 +1,20 @@
 module Days.Day09 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import           Data.List
+import           Data.Map.Strict      (Map)
+import qualified Data.Map.Strict      as Map
+import           Data.Maybe
+import           Data.Set             (Set)
+import qualified Data.Set             as Set
+import           Data.Vector          (Vector)
+import qualified Data.Vector          as Vec
+import qualified Util.Util            as U
 
-import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
-import Data.Void
+import           Data.Attoparsec.Text
+import           Data.Void
+import           Debug.Trace          (trace)
+import qualified Program.RunDay       as R (Day, runDay)
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +22,49 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = (signed decimal `sepBy1` char ' ') `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [[Int]]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA =
+  foldl
+    ( \acc x ->
+        let withDiffs = x : getDiffs x
+            next =
+              foldr
+                (\line acc -> last line + acc)
+                0
+                withDiffs
+         in acc + next
+    )
+    0
+
+getDiffs :: [Int] -> [[Int]]
+getDiffs x =
+  if all (== 0) d
+    then [d]
+    else d : getDiffs d
+ where
+  d = zipWith (flip (-)) x (tail x)
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB =
+  foldl
+    ( \acc x ->
+        let withDiffs = x : getDiffs x
+            next =
+              foldr
+                (\line acc -> head line - acc)
+                0
+                withDiffs
+         in acc + next
+    )
+    0
